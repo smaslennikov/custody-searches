015e0eedf08fdaad546d398f6cd0420bb9b0a773
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2023-04-07 05:58:31 -0700

    Initial commit

---
+++ jefferson.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl https://gisweb.jeffcowa.us/NewWorld.InmateInquiry/Jefferson
---
+++ kitsap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+OPENSSL_CONF=openssl.conf curl 'https://incustody.kitsap.gov/Home/BookingSearchResult?LastName=%25&FirstName=&BookingFrom=&BookingTo=&ValidSearch=&sort=BookingDate&sortdir=DESC'
